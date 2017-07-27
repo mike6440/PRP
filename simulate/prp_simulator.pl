28a91dd33b05e4c9ab13ff0607ff9981e3e90688
Michael Reynolds <michael@rmrco.com>
2017-07-27 11:49:25 -0700

    prp11 improve simulate

--- simulate/prp_simulator.pl
+++ simulate/prp_simulator.pl
@@ -1,6 +1,6 @@
 #! /usr/bin/perl
 
-use lib $ENV{DAQLIB};
+use lib $ENV{MYLIB};
 use perltools::MRtime;
 #use Device::SerialPort qw( :PARAM :STAT 0.07 );
 
@@ -13,10 +13,6 @@ $narg = $#ARGV;
 if($narg>=0){
 	foreach(@ARGV){print"  $_\n"}
 }
-# $p=FindUSBPort();
-# print"port=$p\n";
-# 
-# die;
 
 while (1) {
 	$irec = 0;
@@ -33,14 +29,4 @@ while (1) {
 	}
 	print"Starting over\n";
 	close F;
-}
-
-sub FindUSBPort {
-	my @w = `ls /dev/tty*PL* 2>/dev/null`;
-	my @x = `ls /dev/tty*USB* 2>/dev/null`;
-	my @y = `ls /dev/tty*usb* 2>/dev/null`;
-	@w=(@w,@x,@y);
-	if ( $#w < 0 ){print"No USB\n"; exit 1}
-	chomp($w[0]);
-	return $w[0];
 }
